236fa91f6324fc7dcbff2607d8523735c1f5a7f4
Russell Michell <russ@theruss.com>
2015-05-07 13:55:59 +1200

    NEW: Added test for SecuredFilesystem::get_numeric_identifier().

---
+++ tests/SecuredFilesystemTest.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * 
+ * @author Deviate Ltd 2014-2015 http://www.deviate.net.nz
+ * @package silverstripe-advancedassets
+ */
+class SecuredFilesystemTest extends SapphireTest {
+    
+    /**
+     * Excercises SecuredFilesystem::get_numeric_identifier().
+     * Ensures expected outputs given a variety of inputs
+     */
+    public function testGetNumericIdentifier() {
+        $controller = $this->getTestController(array('ID' => 40), 'GET');
+        $result = SecuredFilesystem::get_numeric_identifier($controller, 'ID');
+        $this->assertInternalType('integer', $result);
+        $this->assertEquals(40, $result);
+        
+        $controller = $this->getTestController(array('ParentID' => 40), 'GET');
+        $result = SecuredFilesystem::get_numeric_identifier($controller, 'ParentID');
+        $this->assertInternalType('integer', $result);
+        $this->assertEquals(40, $result);
+        
+        $controller = $this->getTestController(array('DUMMY' => 40), 'GET');
+        $result = SecuredFilesystem::get_numeric_identifier($controller, 'ID');
+        $this->assertInternalType('integer', $result);
+        $this->assertEquals(0, $result);
+    }
+    
+    /**
+     * 
+     * @param array $vars
+     * @param string $method 
+     * @return Controller
+     */
+    private function getTestController($vars, $method) {
+        $controller = Controller::create();
+        if($method === 'post') {
+            $getVars = array();
+            $postVars = $vars;
+        } else {
+            $getVars = $vars;
+            $postVars = array();
+        }
+        
+        $request = new SS_HTTPRequest($method, '/admin/assets', $getVars, $postVars);
+        $controller->setRequest($request);
+        return $controller;
+    }
+}
