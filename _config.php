843a535545bb1666e4198e0705b784fc255f83c5
Magnus Bengtsson <magnus.bengtsson@deviate.net.nz>
2019-04-12 14:50:26 +1200

    Updated modules that supports php 7.2

--- _config.php
+++ _config.php
@@ -5,6 +5,10 @@
  * @package silverstripe-advancedassets
  */
 
+// Ensure compatibility with PHP 7.2 ("object" is a reserved word),
+// with SilverStripe 3.6 (using Object) and SilverStripe 3.7 (using SS_Object)
+if (!class_exists('SS_Object')) class_alias('Object', 'SS_Object');
+ 
 define("SECURED_FILES_MODULE_NAME", "Advanced Assets");
 define("SECURED_FILES_MODULE_DIR", "silverstripe-advancedassets");
 define("SECURED_FILES_ASSET_SUBDIR", "_securedfiles");
@@ -17,7 +21,7 @@ if(!file_exists(BASE_PATH . DIRECTORY_SEPARATOR . SECURED_FILES_MODULE_DIR)) {
 }
 
 // Overload use of default {@link AssetAdmin} with the module's own {@link NonSecuredAssetAdmin}.
-Object::useCustomClass('AssetAdmin', 'NonSecuredAssetAdmin', true);
+SS_Object::useCustomClass('AssetAdmin', 'NonSecuredAssetAdmin', true);
 
 CMSMenu::remove_menu_item('AssetAdmin');
 CMSMenu::remove_menu_item('CMSSecuredFileAddController');
